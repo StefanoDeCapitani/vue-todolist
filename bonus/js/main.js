b5505812b7d57925d16660104dc95c56a6dfae95
Stefano De Capitani <stefano.deca@live.com>
2021-11-02 21:07:25 +0100

    Tags in tasks + tags filtering

--- bonus/js/main.js
+++ bonus/js/main.js
@@ -6,38 +6,54 @@ Aggiungere un filtro che permetta di cercare un elemento in base al testo, tenen
 
 Vue.config.devtools = true;
 
+const tasks = [
+  {
+    text: "Fare la spesa",
+    checked: false,
+    tagIndex: 1,
+    color: "#ff0000",
+  },
+  {
+    text: "Giocare",
+    checked: false,
+    tagIndex: 1,
+    color: "#ff0000",
+  },
+  {
+    text: "Mangiare",
+    checked: false,
+    tagIndex: 1,
+    color: "#ff0000",
+  },
+  {
+    text: "Studiare",
+    checked: false,
+    tagIndex: 1,
+    color: "#ff0000",
+  },
+];
+
+const tags = [
+  { id: 0, name: "All tags", color: "#6c757d" },
+  { id: 1, name: "Lavoro", color: "#ff0000" },
+  { id: 2, name: "Famiglia", color: "#00ff00" },
+  { id: 3, name: "Tempo Libero", color: "#0000ff" },
+];
+
 window.addEventListener("DOMContentLoaded", function () {
   const vm = new Vue({
     el: "#root",
     data: {
       tabs: ["Crea", "Filtra"],
       selectedTabIndex: 0,
+
+      tasksList: [...tasks],
+      tagsList: [...tags],
+
       inputTask: "",
-      inputColor: "#ff0000",
+      selectedTag: tags[0],
       inputIsValid: true,
       invalidFeedback: "",
-      tasksList: [
-        {
-          text: "Fare la spesa",
-          checked: false,
-          color: "#ff0000",
-        },
-        {
-          text: "Giocare",
-          checked: false,
-          color: "#ff0000",
-        },
-        {
-          text: "Mangiare",
-          checked: false,
-          color: "#ff0000",
-        },
-        {
-          text: "Studiare",
-          checked: false,
-          color: "#ff0000",
-        },
-      ],
 
       inputSearch: "",
       filteredList: [],
@@ -45,41 +61,70 @@ window.addEventListener("DOMContentLoaded", function () {
     methods: {
       selectTab(tabIndex) {
         this.selectedTabIndex = tabIndex;
-        filterTasks();
+        this.filterTasks();
       },
       isSelectedTab(tabIndex) {
         return this.selectedTabIndex === tabIndex;
       },
+
       createTask(inputText) {
         return {
           text: inputText,
           checked: false,
-          color: this.inputColor,
+          tagIndex: this.selectedTag.id,
+          color: this.selectedTag.color,
         };
       },
       onClickAddTask() {
-        inputText = this.inputTask.trim();
+        let inputText = this.inputTask.trim();
+
         taskIsDuplicate = this.tasksList.some(
-          (el) => el.text.toLowerCase() === inputText.toLowerCase()
+          (el) =>
+            el.text.toLowerCase() === inputText.toLowerCase() &&
+            el.tagIndex === this.selectedTag.id
         );
-        if (this.inputTask && !taskIsDuplicate) {
+
+        if (this.inputTask && !taskIsDuplicate && this.selectedTag.id !== 0) {
           this.tasksList.push(this.createTask(inputText));
           this.inputTask = "";
         } else {
           this.inputIsValid = false;
           this.invalidFeedback = taskIsDuplicate
             ? "Non è possibile aggiungere un task già esistente"
-            : "Non è possibile aggiungere un task vuoto, scrivi qualcosa";
+            : "Non è possibile aggiungere un task vuoto o senza tag";
         }
       },
+
       onClickCloseTask(i) {
         this.tasksList.splice(i, 1);
       },
+      onClickCloseFilteredTask(i) {
+        taskIndex = this.tasksList.indexOf(this.filteredList[i]);
+        this.tasksList.splice(taskIndex, 1);
+        this.filterTasks();
+      },
+
+      onClickSelectTag(index) {
+        this.selectedTag = this.tagsList[index];
+        this.inputIsValid = true;
+      },
+      onClickSelectFilterTag(index) {
+        this.selectedTag = this.tagsList[index];
+        console.log(this.selectedTag);
+        this.filterTasks();
+      },
+
       filterTasks() {
-        this.filteredList = this.tasksList.filter((el) =>
-          el.text.toLowerCase().includes(this.inputSearch.toLowerCase())
-        );
+        this.filteredList = this.tasksList.filter((el) => {
+          if (el.text.toLowerCase().includes(this.inputSearch.toLowerCase())) {
+            return this.selectedTag.id === 0
+              ? true
+              : el.tagIndex === this.selectedTag.id;
+          }
+        });
       },
     },
   });
 });
+
+/*  */
